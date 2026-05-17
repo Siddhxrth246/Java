6a06e077d7e6e314f365eb06b5485e64d0e63f3f
Siddhxrth246 <siddharthpatel2252@gmail.com>
2026-05-17 18:06:08 +0530

    h

---
+++ E.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class E {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = 3.141592653 ;
+        double r = sc.nextDouble();
+
+        System.out.println(a * (r*r));
+        sc.close();
+
+
+    }
+}
